73bf64b457f2c2426a0114f2ab2e2ed76688810b
tymancjo <tomasz.tomanek@gmail.com>
2021-10-31 18:22:38 +0100

    ik model 1st working version

--- Python/ik_develop.py
+++ Python/ik_develop.py
@@ -4,7 +4,7 @@
 # MIT license 
 
 import math
-debug = 1
+import matplotlib.pyplot as plt
 
 def my_print(input_msg):
     if debug:
@@ -12,7 +12,7 @@ def my_print(input_msg):
     else:
         ...
 
-def leg_IK_mk2(point, A=17,C=20.54,E=11.25,F=35,G=60,H=90,N=55, be_precise=False):
+def leg_IK_mk2(point, A=17,C=20.54,E=10.25,F=35,G=60,H=90,N=55,D=20.45,B=17,M=35,L=70, be_precise=False):
     """
     Use all dimensions in [mm]
     solwing for the Alpha0, Alpha, Phi - returned in degrees.
@@ -73,13 +73,51 @@ def leg_IK_mk2(point, A=17,C=20.54,E=11.25,F=35,G=60,H=90,N=55, be_precise=False
 
         Alpha0 = math.pi - k1 - k2 - k3
 
+        # the part for the second servo
+        my_print("2nd servo")
+        p2 =  E**2 + D**2
+        p = math.sqrt( p2 )
+        k1 = math.acos( E/p )
+        my_print(f"k1={math.degrees(k1)}")
+        eta = math.pi - Psi - dzetta - k1
+        my_print(f"eta={math.degrees(eta)}")
+
+        R2 = p2 + H**2 -2*p*H*math.cos(eta)
+        R = math.sqrt( R2 )
+        my_print(f"{R=}")
+        k6 = math.acos((H**2+R2 - p2)/(2*H*R))
+        my_print(f"k6={math.degrees(k6)}")
+
+        k2 = math.pi - Phi
+        my_print(f"k2={math.degrees(k2)}")
+        k5 = k2 - k6
+        my_print(f"k5={math.degrees(k5)}")
+
+        Q2 = R2 + M**2 -2*R*M*math.cos(k5)
+        Q = math.sqrt( Q2 )
+        my_print(f"{Q=}")
+        Hy = E + H*math.cos(Psi+dzetta)
+        my_print(f"{Hy=}")
+
+        kd   = math.acos((Q2+B**2-L**2)/(2*Q*B))
+        my_print(f"k.={math.degrees(kd)}")
+        kdd  = math.acos((Q2+R2-M**2)/(2*Q*R))
+        my_print(f"k..={math.degrees(kdd)}")
+        kddd = math.acos((Hy)/(R))
+        my_print(f"k...={math.degrees(kddd)}")
+
+        Beta = kd + kdd + kddd + math.pi/2
+
+
+
+
 
     else:
         return False
     
     if not be_precise:
-        return int(math.degrees(Alpha0)), int(math.degrees(Alpha)), int(math.degrees(Phi))
-    return (math.degrees(Alpha0)), (math.degrees(Alpha)), (math.degrees(Phi))
+        return int(math.degrees(Alpha0)),int(math.degrees(Beta)), int(math.degrees(Alpha)), int(math.degrees(Phi))
+    return (math.degrees(Alpha0)),(math.degrees(Beta)), (math.degrees(Alpha)), (math.degrees(Phi))
 
 
 
@@ -87,4 +125,27 @@ def leg_IK_mk2(point, A=17,C=20.54,E=11.25,F=35,G=60,H=90,N=55, be_precise=False
 debug = 0
 
 # print(leg_IK_mk2((27.43,137.44)))
-print(leg_IK_mk2((-39,126)))
+print(leg_IK_mk2((-33.3,124.6)))
+print(leg_IK_mk2((10,130)))
+print(leg_IK_mk2((0,130)))
+
+x = [k for k in range(50,-50,-5)]
+y = [120 + 10 * math.sin(math.radians(4*k)) for k in x]
+
+print(x, y)
+alfa = []
+beta = []
+
+for xx,yy in zip(x,y):
+    a,b,c,d = leg_IK_mk2((xx,yy))
+    alfa.append(a)
+    beta.append(b-(90+30))
+
+# fig1 = plt.figure()
+plt.plot(x,alfa)
+plt.plot(x,beta)
+plt.plot(x,y)
+plt.xlabel("x [mm]")
+plt.ylabel("Angle [deg]")
+plt.grid()
+plt.show()
